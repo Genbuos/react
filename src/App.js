7c47acaf7f2ebbacd39baa5c0611f88a0edf4b78
Genbuos <swiftypython18@gmail.com>
2022-11-29 17:40:28 -0500

    m

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 
-
-
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 function App() {
-  return (
-    <div >
+  return (<BrowserRouter>
+    <div className='d-flex flex-column site-container'>
       <header >
-        <a href="/">Sheco Essential Organics</a>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <LinkContainer to="/">
+        <Navbar.Brand>Sheco Essential Organics</Navbar.Brand>
+        </LinkContainer>
+         
+        </Container>
+      </Navbar>
+   
       </header>
       <main>
-        list products
+      <Container>
+      <Routes>
+      <Route path="/product/:slug" element={<ProductScreen/>}/>
+        <Route path="/" element={<HomeScreen/>}/> 
+      </Routes>
+      </Container>
+        
+        
       </main>
+      <footer>
+        <div className="text-center">All Rights Reserved</div>
+      </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = {
+    products: [
+        {
+            name: 'Sea Moss Gel',
+            slug: "sea-moss-gel",
+            category: 'sea-moss',
+            image: '/images/seamoss.jpeg', // 679px x 829px
+            price: 120,
+            countInStock: 10,
+            brand: 'SheBoss',
+            description: 'high quality seamoss',
+        },
+        {
+            name: 'Sea Moss Juice',
+            slug: "sea-moss-juice",
+            category: 'sea-moss',
+            image: '/images/RawSeaMoss.jpeg',
+            price: '250',
+            countInStock: 10,
+            description: 'Seamoss infused drink.. flavour of your choice.'
+        }, 
+    ],
+};
+
+export default data;
