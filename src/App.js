19e2f49c4def6ebff2635b98f9099159f730ea77
Genbuos <swiftypython18@gmail.com>
2022-11-29 13:54:41 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">Sheco Essential Organics</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
