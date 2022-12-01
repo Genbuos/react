c33a9e072a75696cd8f5239f818a40764bf976e9
Genbuos <swiftypython18@gmail.com>
2022-11-30 20:25:58 -0500

    bit of functionality

--- src/components/product.js
+++ src/components/product.js
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
+import { Store } from "../Store";
+import {useContext} from 'react';
+import axios from "axios";
 function Product(props) {
   const { product } = props;
+  const {state, dispatch: ctxDispatch} = useContext(Store);
+    const  {cart} = state;
+    const addToCartHandler = async() =>{ 
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if(data.countInStock < quantity) {
+          window.alert('Sorry. Product is out of stock');
+          return;
+        }
+      ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity: 1}
+    });
+
+    };
   return (
     <Card >
       <Link to={`/product/${product.slug}`}>
@@ -14,7 +31,7 @@ function Product(props) {
         </Link>
         
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add to cart</Button>
+        <Button onClick={addToCartHandler}>Add to cart</Button>
       </Card.Body>
 
       <div className="product-info">
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import ListGroup from "react-bootstrap/ListGroup"
@@ -28,6 +28,7 @@ const reducer = (state, action) => {
   
   
 function ProductScreen() {
+  const navigate = useNavigate();
     const params = useParams();
     const {slug} = params;
     const [{loading, error, product}, dispatch] = useReducer(reducer,{
@@ -47,10 +48,18 @@ function ProductScreen() {
     },
     [slug]);
     const {state, dispatch: ctxDispatch} = useContext(Store);
-    const addToCartHandler = () =>{ 
-      ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity: 1},
+    const  {cart} = state;
+    const addToCartHandler = async () =>{ 
+        const existItem = cart.cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${product._id}`);
+        if(data.countInStock < quantity) {
+          window.alert('Sorry. Product is out of stock');
+          return;
+        }
+      ctxDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity},
     });
-
+      navigate('/cart');
     };
     return (
         error? (<MessageBox variant="danger">{error}</MessageBox>)
